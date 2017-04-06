1c4dc11c250558f8e6e6b985e22ad6e8bcca3fad
darkAco <lejon.doering@googlemail.com>
2017-04-06 14:56:35 +0200

    Update FileTransferServer.py

--- FileTransferServer.py
+++ FileTransferServer.py
@@ -8,49 +8,40 @@ import pickle
 import socket
 import threading
 import hashlib
-from time import sleep
+import time
+import random
 
-# Helper Functions // Hilfsfunktionen
-def checkSocket(sock):
-    testsock = socket.socket(sock.family, sock.type)
-    adrtup = ('127.0.0.1', sock.getsockname()[1])
-    print("Testing connection to", adrtup)
-    retval = testsock.connect_ex(adrtup)
-    print("Retval", retval)
-    if retval is 0:
-        testsock.shutdown(socket.SHUT_RDWR)
-        testsock.close()
-    del testsock
-    return True if retval is 0 else False
-
+"""
+    Helper Functions // Hilfsfunktionen
+"""
+# Calculates the hash value of the given bytes // Berechnet den Hashwert der Bytefolge
 def hashBytes(bytestring):
+    hashlib.sha256()
     return hashlib.sha256(bytestring).digest()
 
-# Main-Function // Hauptroutine
-if __name__ == '__main__':
+# Encodes the bytestring "message" with "key" by applying Vigenère encryption
+# Warning: Insecure Encryption! This is only used for obfuscation
+def encodedBytes(key, message):
+    res = []
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        res.append(chr(ord(message[i]) + ord(key_c) % 256))
+    return "".join(res)
+
+# Check if the executing OS is compatible 
+def syscheck():
+    if platform.system() != "Windows" and platform.system() != "Linux":
+        raise Exception("[ERROR]\tOperating System could not be determined. Exiting.")
+        exit(1)
+
+# Loads the (relative) local config file
+def loadConfig(confpath=r'ftsrv.conf'):
     conf = dict()
-    confpath = r'server.conf'
-    opsys = platform.system()
-
-    # I.: System Check
-    if opsys is "Windows":
-        print("Running on a Windows Machine")
-    elif opsys is "Linux":
-        print("Running on a Linux Machine")
-    elif opsys is "Java":
-        raise Exception("Running in a Java Virtual Environment is not supported (yet). Exiting.")
-        exit(1)
-    else:
-        raise Exception("Operating System could not be determined. Exiting.")
-        exit(1)
-
-    # II.: Load Config
-    # Config File is located under {script root path}\config.bin
     if os.path.exists(confpath):
         conf = pickle.load(open(confpath, 'rb'))
-        print("Configuration file loaded")
+        print("[INFO]\tConfiguration file loaded")
     else:
-        print("Configuration file was not found, using default config")
+        print("[INFO]\tConfiguration file was not found, using default config")
         conf = dict()
         conf.update({"L3PROT":"tcp"})
         conf.update({"HOSTNAME":"localhost"})
@@ -58,78 +49,79 @@ if __name__ == '__main__':
         conf.update({"HOSTPORT":5555})
     for key in conf:
         print(key, ":\t", conf[key])
+    return conf
 
-    # III.: Startup
+# Processes a customary three way handshake with the client
+# (only to be called on active connections)
+def ThreeWayHandshake(clSock, clAdr):
+    # Step 1
+    data = clSock.recv(2024).decode('utf-8')
+    # Check correct greeting (ServerHostname)
+    if data != socket.gethostname() or not data:
+        raise Exception("[ERROR]\tHandshake Failed at Step 1 ("+data+"!="+socket.gethostname()+")")
+    else:
+        print("[INFO]\tHandshake Step 1 ok")
+    # Step 2
+    # Get random session key from client hash:
+    r = random.Random()
+    clSeed = hashBytes((clAdr[0]+str(time.time())).encode('utf-8'))
+    r.seed(clSeed)
+    sessionKey = ""
+    for i in range(256):
+        sessionKey += chr(r.getrandbits(8))
+    try:
+        print("[DEBUG]\tSessionKey:"+sessionKey)
+        print("[DEBUG]\tSeed:"+clSeed.decode('utf-8'))
+    except Exception:
+        pass
+    # Send sessionkey to Client
+    clSock.send(str(sessionKey).encode('utf-8'))
+    print("[INFO]\tHandshake Step 2 ok")
+    # Step 3
+    data = clSock.recv(2024).decode('utf-8')
+    # Check correct session key Confirmation (encoded ServerHostname)
+    enc_hostname = encodedBytes(str(sessionKey), str(socket.gethostname()))
+    if data != enc_hostname or not data:
+        raise Exception("[ERROR]\tHandshake Failed at Step 3 ("+data+"!="+enc_hostname+")")
+    else:
+        print("[INFO]\tHandshake Step 3 ok\n[INFO]\tHandshake finished")
+
+# Main-Function // Hauptroutine
+if __name__ == '__main__':
+    # Load config
+    conf = loadConfig()
+    # Create socket
     sock = None
     if conf["L3PROT"].lower() == "tcp":
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
     elif conf["L3PROT"].lower() == "udp":
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)
     else:
-        raise Exception("Could not parse L3-Protocol from config, exiting")
+        raise Exception("[ERROR]\tCould not parse L3-Protocol from config, exiting")
         exit(1)
     sockAdrTup = (conf["HOSTIP"], conf["HOSTPORT"])
     sock.bind(sockAdrTup)
-    print("Socket bound at", sockAdrTup)
+    print("[INFO]\tSocket created and bound at", sockAdrTup)
     
-    # IV.: Main Serving Loop
+    # Listen on Socket
     sock.listen(1)
-    serving = True
-    chunksize = conf["BUFFERSIZE"]
-    greetings = ["Intellect is the Understanding of Knowledge.",
-                 "Sentience is the Basest Form of Intellect.",
-                 "Understanding is the True Path to Comprehension.",
-                 "Comprehension is the Key to all Things.",
-                 "The Omnissiah knows all, comprehends all.",
-                 "The Soulless sentience is the enemy of all.",
-                 "Flesh is Fallible, but Ritual Honours the Machine Spirit.",
-                 "To Break with Ritual is to Break with Faith."]
-    while serving:
-        # Await Connection        
+    while True:
+        print("[INFO]\tawaiting Connection ..")
         remoteSock, remoteAdr = sock.accept()
-        print("Connection established from", remoteAdr)
+        print("[INFO]\tConnection established from", remoteAdr)
         handling = True
         try:
             while handling:
                 if conf["L3PROT"].lower() == "tcp":
-                    # Protocol dictates how handling has to be done for tcp
-                    # 1. Client connects to server (done here)
-                    # 2. Client sends (Server receives) ALL correct greetings hashed:
-                    recv_greetings = []
-                    for i in range(len(greetings)):
-                        data = remoteSock.recv(64) # 64 is Fixed Size for greetings
-                        msg = data.decode('utf-8')
-                        print(msg, str(msg==greetings[i]))
-                        recv_greetings.append(msg)
-                    for i in range(len(greetings)):
-                        if greetings[i] != recv_greetings[i]:
-                            raise Exception("Incorrect greeting")
-                    # Greeting phase succeeded here
-                    connectionStanding = True
-                    # 3. Wait for file requests or offers
-                    while connectionStanding:
-                        data = remoteSock.recv(64) # 64 is fixed size for commands
-                        msg = data.decode('utf-8')
-                        # Send or Receive File?
-                        if "CliSend" == msg:
-                            print("Receiving file from Client ...")
-                            data = remoteSock.recv(512) # 512 is fixed size for path+filename
-                            filepath = data.decode('utf-8')
-                            data = remoteSock.recv(8)   # 8 Byte for boolean override True/False
-                            override = bool(data.decode('utf-8'))
-
-                            # Answer to client, confirmation to send file
-                            if os.path.exists(filepath) and not override:
-                                print("File already existing, not receiving.")
-                                remoteSock.sendall("False".encode("utf-8"))
-                            else: # can write
-                                fh = open(filepath, 'wb+')
-                        elif "SrvSend" == msg:
-                            print("Sending file to Client ...")
+                    # Server greets Client with Version hash
+                    # Build up Connection (Handshake)
+                    ThreeWayHandshake(remoteSock, remoteAdr)
+                    raise Exception("[DEBUG]\tExiting after successful Three-Way-Handshake")
+                
                 elif conf["L3PROT"].lower() == "udp":
-                    raise Exception("Not yet implemented, use tcp for now.")
+                    raise Exception("[ERROR]\tUDP is not yet implemented, use tcp for now.")
         except Exception as e:
-            print("Error:", e)
+            print(str(time.time()) + "\n" + str(e))
         finally:
             remoteSock.close()
     # Shutdown socket
